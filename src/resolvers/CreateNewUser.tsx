ae8083cfdf7c9dc30f311d26b1c7e78cba13c1c6
Tyler Blovat <tyler.blovat@uconn.edu>
2020-09-07 09:54:37 -0400

    User creation working properly.

--- src/resolvers/CreateNewUser.tsx
+++ src/resolvers/CreateNewUser.tsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { graphql } from "react-apollo";
 import { flowRight as compose } from "lodash";
 import { createUserMutation } from "../queries/queries.js";
+import { browserHist } from "../AppMain/history.js";
+import { statusContext } from "../AppMain/App";
 
 interface Props {
   username: string;
   password: string;
-  createUserMutation: (variables: object) => void;
+  createUserMutation: (variables: object) => any;
 }
 
 const CreateNewUser: React.FC<Props> = (props) => {
+  const { status, setStatus } = useContext(statusContext);
+
   function submitButton() {
     let userId = Math.floor(Math.random() * 1000000);
-    props.createUserMutation({
-      variables: {
-        userId: userId,
-        username: props.username,
-        password: props.password,
-        money: 1000,
-      },
-    });
+    props
+      .createUserMutation({
+        variables: {
+          userId: userId,
+          username: props.username,
+          password: props.password,
+          money: 1000,
+          darkmode: false,
+          invisible: false,
+          allowCommentsOnTrades: true,
+        },
+      })
+      .then((res: string) => {
+        console.log("success");
+        setStatus(true);
+        browserHist.push("/");
+      })
+      .catch((res: string) => {
+        console.log("error");
+      });
   }
 
   return <button onClick={() => submitButton()}>Create Account</button>;
