7f7469d2a384c0d0a9b0bdd5444b87e3370a1214
Tyler Blovat <tyler.blovat@uconn.edu>
2020-09-16 22:36:10 -0400

    Added data to feed, refactored a bit of code, added some styling.

--- src/Homepage/Feed.tsx
+++ src/Homepage/Feed.tsx
@@ -3,28 +3,80 @@ import { PostType, NewsType, CommentType } from "./FeedTypes";
 
 const Feed: React.FC = () => {
   const testData = [
-    { typeId: 0, data: { title: "title 1" } },
-    { typeId: 1, data: { title: "title 2" } },
-    { typeId: 2, data: { user: "Tyler" } },
+    {
+      typeId: 0,
+      data: {
+        title: "Analysis of the Apple announcement",
+        text:
+          "We can see by the Apple announcement that they clearly don't care about their base users, and are only really chasing clout.",
+        user: "Tyler",
+        timestamp: 2312424535,
+        likes: 15,
+        dislikes: 6,
+        replies: 4,
+      },
+    },
+    {
+      typeId: 1,
+      data: {
+        headline: "Apple Announces New Chip",
+        name: "Apple",
+        ticker: "AAPL",
+        subtext:
+          "On September 23, Apple announced a new chip to rival those of Intel and Nvidia.",
+      },
+    },
+    {
+      typeId: 2,
+      data: {
+        user: "Tyler",
+        text:
+          "Apple sucks. Nvidia's chips will be better because they specialize.",
+        timestamp: 1423123531,
+        likes: 3,
+        dislikes: 2,
+        replies: 2,
+      },
+    },
   ];
 
   function conditionalRender(id: number, data: any) {
     if (id === 0) {
       return (
         <div>
-          <PostType title={data.title} />
+          <PostType
+            title={data.title}
+            user={data.user}
+            text={data.text}
+            timestamp={data.timestamp}
+            likes={data.likes}
+            dislikes={data.dislikes}
+            replies={data.replies}
+          />
         </div>
       );
     } else if (id === 1) {
       return (
         <div>
-          <NewsType title={data.title} />
+          <NewsType
+            headline={data.title}
+            name={data.name}
+            ticker={data.ticker}
+            subtext={data.subtext}
+          />
         </div>
       );
     } else if (id === 2) {
       return (
         <div>
-          <CommentType user={data.user} />
+          <CommentType
+            user={data.user}
+            text={data.text}
+            timestamp={data.timestamp}
+            likes={data.likes}
+            dislikes={data.dislikes}
+            replies={data.replies}
+          />
         </div>
       );
     }
--- src/Homepage/FeedTypes.tsx
+++ src/Homepage/FeedTypes.tsx
@@ -2,20 +2,42 @@ import React from "react";
 
 interface Post {
   title: string;
+  user: string;
+  text: string;
+  timestamp: number;
+  likes: number;
+  dislikes: number;
+  replies: number;
 }
 
 interface News {
-  title: string;
+  headline: string;
+  ticker: string;
+  name: string;
+  subtext: string;
 }
 
 interface Comment {
   user: string;
+  text: string;
+  timestamp: number;
+  likes: number;
+  dislikes: number;
+  replies: number;
 }
 
 export const PostType: React.FC<Post> = (props) => {
   return (
     <div>
       <h2>{props.title}</h2>
+      <h4>
+        Posted by {props.user} at {props.timestamp}
+      </h4>
+      <p>
+        likes: {props.likes}, dislikes: {props.dislikes}, shares:{" "}
+        {props.replies}
+      </p>
+      <p>{props.text}</p>
     </div>
   );
 };
@@ -23,7 +45,11 @@ export const PostType: React.FC<Post> = (props) => {
 export const NewsType: React.FC<News> = (props) => {
   return (
     <div>
-      <h2>{props.title}</h2>
+      <h2>
+        {props.name} #{props.ticker}
+      </h2>
+      <h2>{props.headline}</h2>
+      <p>{props.subtext}</p>
     </div>
   );
 };
@@ -32,6 +58,11 @@ export const CommentType: React.FC<Comment> = (props) => {
   return (
     <div>
       <h3>{props.user}</h3>
+      <p>{props.text}</p>
+      <p>{props.timestamp}</p>
+      <p>
+        likes: {props.likes} dislikes: {props.dislikes} shares: {props.replies}
+      </p>
     </div>
   );
 };
