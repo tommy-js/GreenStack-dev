52c78e6a4b736c7962d51a9f80dca87404d54645
Tyler Blovat <tyler.blovat@uconn.edu>
2020-09-23 16:33:53 -0400

    Like, dislike system working on comments.

--- src/Homepage/IndividualComment.tsx
+++ src/Homepage/IndividualComment.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   username: string;
@@ -9,13 +9,61 @@ interface Props {
 }
 
 const IndividualComment: React.FC<Props> = (props) => {
+  const [stateLike] = useState(props.likes);
+  const [stateDislike] = useState(props.dislikes);
+  const [likes, setLikes] = useState(props.likes);
+  const [dislikes, setDislikes] = useState(props.dislikes);
+
+  function like() {
+    let updateLike;
+
+    if (stateDislike === dislikes) {
+      if (stateLike === likes) {
+        updateLike = stateLike + 1;
+        setLikes(updateLike);
+      } else {
+        setLikes(stateLike);
+      }
+    } else {
+      if (stateLike === likes) {
+        updateLike = stateLike + 1;
+        setLikes(updateLike);
+        setDislikes(stateDislike);
+      } else {
+        setLikes(stateLike);
+      }
+    }
+  }
+
+  function dislike() {
+    let updateDislike;
+
+    if (stateLike === likes) {
+      if (stateDislike === dislikes) {
+        updateDislike = stateDislike + 1;
+        setDislikes(updateDislike);
+      } else {
+        setDislikes(stateDislike);
+      }
+    } else {
+      if (stateDislike === dislikes) {
+        updateDislike = stateDislike + 1;
+        setDislikes(updateDislike);
+        setLikes(stateLike);
+      } else {
+        setDislikes(stateDislike);
+      }
+    }
+  }
+
   return (
     <div id="comment">
       <p id="comment_name">{props.username} </p>
       <p id="comment_time">posted at {props.timestamp}</p>
       <p id="comment_text">{props.text}</p>
       <p id="comment_information">
-        likes: {props.likes}, dislikes: {props.dislikes}
+        {likes} <div onClick={() => like()}>upvote</div>, {dislikes}{" "}
+        <div onClick={() => dislike()}>downvote</div>,
       </p>
     </div>
   );
