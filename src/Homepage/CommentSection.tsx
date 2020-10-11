8c231889c8d6131681702313ca2144ee122e4042
Tyler Blovat <tyler.blovat@uconn.edu>
2020-10-11 11:49:46 -0400

    Component sorting on the comment section of posts

--- src/Homepage/CommentSection.tsx
+++ src/Homepage/CommentSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import IndividualComment from "./IndividualComment";
 
 interface Props {
@@ -12,20 +12,39 @@ interface Props {
 }
 
 const CommentSection: React.FC<Props> = (props) => {
-  return (
-    <div id="comment_section">
-      {props.comments.map((el: any) => (
-        <IndividualComment
-          key={el.timestamp}
-          username={el.username}
-          text={el.text}
-          timestamp={el.timestamp}
-          likes={el.likes}
-          dislikes={el.dislikes}
-        />
-      ))}
-    </div>
-  );
+  const [comments, setComments] = useState();
+
+  useEffect(() => {
+    props.comments.sort(function (a, b) {
+      return b.timestamp - a.timestamp;
+    });
+    setComments(props.comments);
+  }, []);
+
+  useEffect(() => {
+    console.log(comments);
+  }, [comments]);
+
+  function returnRender() {
+    if (comments) {
+      return (
+        <div id="comment_section">
+          {props.comments.map((el: any) => (
+            <IndividualComment
+              key={el.timestamp}
+              username={el.username}
+              text={el.text}
+              timestamp={el.timestamp}
+              likes={el.likes}
+              dislikes={el.dislikes}
+            />
+          ))}
+        </div>
+      );
+    } else return null;
+  }
+
+  return <div>{returnRender()}</div>;
 };
 
 export default CommentSection;
--- src/Homepage/post/PostComments.tsx
+++ src/Homepage/post/PostComments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import IndividualPostComment from "./IndividualPostComment";
 
 interface Props {
@@ -14,21 +14,40 @@ interface Props {
 }
 
 const PostComments: React.FC<Props> = (props) => {
-  return (
-    <div>
-      {props.comments.map((el: any) => (
-        <IndividualPostComment
-          userId={el.userId}
-          username={el.username}
-          commentId={el.commentId}
-          timestamp={el.timestamp}
-          text={el.text}
-          likes={el.likes}
-          dislikes={el.dislikes}
-        />
-      ))}
-    </div>
-  );
+  const [comments, setComments] = useState();
+
+  useEffect(() => {
+    props.comments.sort(function (a, b) {
+      return a.timestamp - b.timestamp;
+    });
+    setComments(props.comments);
+  }, []);
+
+  useEffect(() => {
+    console.log(comments);
+  }, [comments]);
+
+  function returnRender() {
+    if (comments) {
+      return (
+        <div>
+          {comments.map((el: any) => (
+            <IndividualPostComment
+              userId={el.userId}
+              username={el.username}
+              commentId={el.commentId}
+              timestamp={el.timestamp}
+              text={el.text}
+              likes={el.likes}
+              dislikes={el.dislikes}
+            />
+          ))}
+        </div>
+      );
+    } else return null;
+  }
+
+  return <div>{returnRender()}</div>;
 };
 
 export default PostComments;
