5aa9aff0377b12ed1d6e0f2333bb87d2be383720
Tyler Blovat <tyler.blovat@uconn.edu>
2020-10-28 13:38:03 -0400

    More work on the Portfolio page, specifically modifying the UI and UX of the owned shares components.

--- src/portfolio/ShareListing.tsx
+++ src/portfolio/ShareListing.tsx
@@ -12,16 +12,15 @@ interface Props {
 const ShareListing: React.FC<Props> = (props) => {
   return (
     <div id="share_listing">
-      <div id="left_share_component">
-        <p>
-          {props.title} ${props.ticker} ({props.shares} shares)
-        </p>
-      </div>
-      <div id="trade_buttons">
-        <Link to={`/home/stock/${props.stockId}`}>
-          <div id="buy_button">Trade</div>
-        </Link>
-      </div>
+      <Link className="no_link" to={`/home/stock/${props.stockId}`}>
+        <div id="left_share_component">
+          <p id="share_listing_text">
+            {props.title} ${props.ticker} (
+            <span id="share_listing_minor">{props.shares} shares</span>)
+          </p>
+        </div>
+        <div id="value_markers">${props.shares * 100}</div>
+      </Link>
     </div>
   );
 };
