df3d89ce28581be0ee5874efb2db02b4d2525a4d
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-21 18:58:37 -0500

    Add styling to new portfolio page.

--- src/portfolio/InitialPortfolio.tsx
+++ src/portfolio/InitialPortfolio.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import portfolio_background from "../images/portfolio_background.png";
 
 const InitialPortfolio: React.FC = () => {
   return (
-    <div>
-      <h2 style={{ position: "relative", top: "200px" }}>
-        Welcome to your portfolio!
+    <div id="initial_portfolio">
+      <h2 id="initial_portfolio_welcome_header">Welcome to your portfolio!</h2>
+      <div id="portfolio_background_container">
+        <img id="portfolio_background" src={portfolio_background} />
+      </div>
+      <h2 id="initial_portfolio_welcome_sub_header">
+        Where You Keep Track of Things
       </h2>
+      <div>
+        <p>Add a stock to your portfolio to get started</p>
+        <input />
+      </div>
     </div>
   );
 };
