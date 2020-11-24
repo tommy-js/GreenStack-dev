b608508a4b20216620bf38cbfe6f1005fb5b8bea
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-24 18:39:56 -0500

    Refactor Explore directory in Homepage

--- src/Homepage/explore/ExploreCompany.tsx
+++ src/Homepage/explore/ExploreCompany.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-interface Props {
-  stockId: number;
+type Company = {
   keyId: number;
+  stockId: string;
   title: string;
   ticker: string;
   description: string;
+};
+
+interface Props {
+  company: Company;
 }
 
-const ExploreCompany: React.FC<Props> = (props) => {
+export const ExploreCompany: React.FC<Props> = ({ company }: Props) => {
   return (
-    <div key={props.keyId} className="homepage_block_component">
-      <Link className="block_link" to={`/home/stock/${props.stockId}`}>
+    <div key={company.keyId} className="homepage_block_component">
+      <Link className="block_link" to={`/home/stock/${company.stockId}`}>
         <p>
-          {props.title} #{props.ticker}
+          {company.title} #{company.ticker}
         </p>
-        <p>{props.description}</p>
+        <p>{company.description}</p>
       </Link>
     </div>
   );
 };
-
-export default ExploreCompany;
