34efe8fb2352f6f00ff454c3e9bcd1027de01395
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-25 15:16:08 -0500

    Refactor FollowingElement directory within Homepage

--- src/Homepage/FollowingElement/FollowingElement.tsx
+++ src/Homepage/FollowingElement/FollowingElement.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import UnfollowUser from "../resolvers/UnfollowUser";
+import React from "react";
+import { UnfollowUser } from "../UnfollowUser/UnfollowUser";
 import { Link } from "react-router-dom";
 
 interface Props {
@@ -8,16 +8,17 @@ interface Props {
   bio: string;
 }
 
-const FollowingElement: React.FC<Props> = (props) => {
+export const FollowingElement: React.FC<Props> = (props) => {
   return (
     <div className="homepage_block_component">
-      <Link className="block_link" to={`/home/user/${props.userId}`}>
-        <p className="element_username">{props.username}</p>
+      <Link
+        className="block_link element_username"
+        to={`/home/user/${props.userId}`}
+      >
+        {props.username}
       </Link>
       <p className="following_element_descriptor">{props.bio}</p>
       <UnfollowUser userId={props.userId} />
     </div>
   );
 };
-
-export default FollowingElement;
---
+++ src/Homepage/UnfollowUser.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { graphql } from "react-apollo";
+import { flowRight as compose } from "lodash";
+import { unfollowUserMutation } from "../queries/queries.js";
+
+interface Props {
+  followerId: number;
+  unfollowUserMutation: (variables: object) => void;
+}
+
+const UnfollowUser: React.FC<Props> = (props) => {
+  function unfollow() {
+    let token = sessionStorage.getItem("Token");
+    props.unfollowUserMutation({
+      variables: {
+        token: token,
+        followerId: props.followerId,
+      },
+    });
+  }
+
+  return <button onClick={() => unfollow()}>unfollow</button>;
+};
+
+export default compose(
+  graphql(unfollowUserMutation, { name: "unfollowUserMutation" })
+)(UnfollowUser);
