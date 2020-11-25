50b3f6d49d8a717e2a95a5f18a559bae9d0ec87a
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-25 17:43:55 -0500

    LearnGlossaryPage.tsx error free

--- src/about/LearnGlossaryPage/LearnGlossaryPage.tsx
+++ src/about/LearnGlossaryPage/LearnGlossaryPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../../navigation/NavBar";
+import { NavBar } from "../../navigation/NavBar/NavBar";
 
 export const LearnGlossaryPage: React.FC = () => {
   return (
