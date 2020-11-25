eaa7a426efc13bea2442e5f3f915ba2d1c158ba4
Tyler Blovat <tyler.blovat@uconn.edu>
2020-11-25 10:55:26 -0500

    Refactor Like directory within Homepage

--- src/Homepage/profile/Like/Like.tsx
+++ src/Homepage/profile/Like/Like.tsx
@@ -5,12 +5,10 @@ interface Props {
   title: string;
 }
 
-const Like: React.FC<Props> = (props) => {
+export const Like: React.FC<Props> = (props) => {
   return (
     <div key={props.postId} className="profile_like">
       <h3>{props.title}</h3>
     </div>
   );
 };
-
-export default Like;
